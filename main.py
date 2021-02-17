7cb2f5bcdb8d7fe9e2d8eb275d45851daccb0f2d
Martin T <martin070891@gmail.com>
2021-02-17 19:02:03 +0000

    Add functions to make a request for the APOD and put it in discord

--- main.py
+++ main.py
@@ -7,17 +7,34 @@ from keep_alive import keep_alive
 
 # Create an instance of a client to connect to discord
 client = discord.Client()
-# Make the token in .env accessible
+# Make items in the .env accessible
 load_dotenv()
 
 # The URL to grab apod from
-URL = "https://api.nasa.gov/planetary/apod"
+URL = "https://api.nasa.gov/planetary/apod?&thumbs=true&api_key="
+# TODO - make URL for returning X random pics
 
 @client.event
 async def on_ready():
     print("logged in as {0.user}".format(client))
     # Set the bot's presence info
     await client.change_presence(activity = discord.Game("with spacetime"))
+
+def getAPOD():
+    response = requests.get(URL + os.getenv("API_KEY"))
+    data = json.loads(response.text)
+    date = data["date"]
+    title = data["title"]
+    explanation = data["explanation"]
+    url = data["url"]
+    # Get the HD image if there is one
+    if "hdurl" in data:
+        hdURL = data["hdurl"]
+        print(hdURL)
+        discordMessage = title + " " + date + "\n\n" + explanation + "\n\n" + hdURL
+    else: # Use the standard image url instead
+        discordMessage = title + " " + date + "\n\n" + explanation + "\n\n" + url
+    return discordMessage
 
 @client.event
 # test function to confirm the bot is working
@@ -28,6 +45,10 @@ async def on_message(message):
     
     if message.content.startswith("!test"):
         await message.channel.send("I'm alive")
+
+    if message.content.startswith("!apod"):
+        apod = getAPOD()
+        await message.channel.send(">>> " + apod)
 
 # Run the web server
 keep_alive()
